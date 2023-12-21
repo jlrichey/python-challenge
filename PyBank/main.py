fb9b742a1f3a73b10fb3a98f7c00e29e55e3b2e0
jlrichey <jrichey@pdmusic.com>
2023-12-21 06:11:48 -0800

    correct/develop comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,90 +1,89 @@
-# In this assignment, you will create a Python script that analyzes the financial records of your company.  Inside your starter code, you will find a financial dataset in the budget_data.csv file. This dataset is composed of two columns, Date and Profit/Losses. (Thankfully, your company has rather lax standards for accounting, so the records are simple.)
-
-# Your task is to create a Python script that analyzes the records to calculate each of the following:
-
+# Import os and csv libraries
 
 import os
 import csv
 
+# Set the path to the csv data file
+
 csvpath = os.path.join("Resources", "budget_data.csv")
 
-# Open the csv
+# Initializing variables for analysis output
+
 greatest_increase = 0
 greatest_decrease = 0
 greatest_increase_date = 0
 greatest_decrease_date = 0
 
+# Open the csv as `csvfile` and read the contents into 'csvreader'
 
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     
 
-    next(csvreader)
-    rowcount = 0 # total number of rows/months in the dataset
-    net_PL = 0 # 
-    change_m2m = []
-    currentrow = 0
-    
-    data = list(csvreader)
-    lastrow = int(data[0][1])
+    next(csvreader) # Function to advance the iterator to remove table header from loops
+    rowcount = 0 # Variable to determine total number of rows/months in the dataset
+    net_PL = 0 # Variable to store the total of P/L
+    change_m2m = [] # List to capture month to month change for average change calculation
+    data = list(csvreader) # Creating list 'data' from 'csvreader'
+    last_row = int(data[0][1]) # Assign first row P/L amount to 'last_row' for first change calculation
+
+# For loop to iterate through 'data', calculate net P/L, and count rows(months) 
+
     for row in data:
-        # rowcount += 1
-        net_PL += int(row[1])
-        
-        
-        # if rowcount != 0:
-        #     change_m2m.append(int(lastrow) + int(row[1]))
-        #     print(f"{int(lastrow)} + {int(row[1])} = ")
-        #     print(int(lastrow) + int(row[1]))
-        #     lastrow = row[1]        
+        net_PL += int(row[1])    
         rowcount += 1
-        
-       
+
+# For loop to calculate month to month P/L changes and append to 'change_m2m' list for average P&L calculation      
+
     for row in data[1:]:
-       
-        change = int(row[1]) - lastrow
+        change = int(row[1]) - last_row
         change_m2m.append(change)
-        lastrow = int(row[1])
-        
+        last_row = int(row[1])
+
+# Calculate average change in P/L from month to month
+
 avg_change = sum(change_m2m) / len(change_m2m)
-print(change_m2m)
-print(max(change_m2m))
+
+# Locate greatest P/L increase and decrease in 'change_m2m' list and assign to analysis variables
 
 greatest_increase = max(change_m2m)
 greatest_decrease = min(change_m2m)
+
+# Gather index from 'change_m2m' list for greatest increase and decrease dates
+
 max_index = change_m2m.index(greatest_increase)
 min_index = change_m2m.index(greatest_decrease)
-print(max_index)
+
+# Offset row index by "+1" to correct for greatest increase and decrease dates
+
 greatest_increase_date = data[max_index + 1][0]
 greatest_decrease_date = data[min_index + 1][0]
+
+# Print analysis to terminal
+
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {rowcount}")
 print(f"Total: ${net_PL}")
 print(f"Average Change: ${round(avg_change, 2)}")
-print(f"Greatest Increase in Profits: {greatest_increase_date} ({greatest_increase})")
-print(f"Greatest Decrease in Profits: {greatest_decrease_date} ({greatest_decrease})")
+print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})\n")
 
-# print(change_m2m)
-# change_m2m_len = len(change_m2m)
-# print(change_m2m_len)
+# Create the output text file for the results
 
-# The net total amount of Profit/Losses over the entire period
+output_path = os.path.join("Resources", "results.txt")
 
-# The average of the changes in Profit/Losses over the entire period
+# Open 'results.txt' file and store the analysis results
 
-# The greatest increase in profits (date and amount) over the entire period
+with open(output_path, 'w') as file:
+    file.write("Financial Analysis\n")
+    file.write("----------------------------\n")
+    file.write(f"Total Months: {rowcount}\n")
+    file.write(f"Total: ${net_PL}\n")
+    file.write(f"Average Change: ${round(avg_change, 2)}\n")
+    file.write(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})\n")
+    file.write(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})\n")
 
-# The greatest decrease in losses (date and amount) over the entire period
+# Print output text file path and file name to the terminal to notify user    
 
-# Your resulting analysis should look similar to the following:
-
-#   Financial Analysis
-#   ----------------------------
-#   Total Months: 86
-#   Total: $38382578
-#   Average  Change: $-2315.12
-#   Greatest Increase in Profits: Feb-2012 ($1926159)
-#   Greatest Decrease in Profits: Sep-2013 ($-2196167)
-
-# Your final script should print the analysis to the terminal and export a text file with the results.
+print(f"The analysis results have been stored as '{output_path}'.")
